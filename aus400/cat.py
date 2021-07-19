12cac24d6a1517b6220dbcf9b1654ed793704f19
Scott Wales <scott.wales@unimelb.edu.au>
2021-07-19 16:56:43 +1000

    Fix height interpolation

--- aus400/cat.py
+++ aus400/cat.py
@@ -72,7 +72,7 @@ def load_catalogue():
     cat = pandas.read_csv(root / "catalogue.csv", parse_dates=["time"])
     var = pandas.read_csv(root / "variables.csv")
 
-    return cat.merge(var, on=["variable", "stream"])
+    return cat.merge(var, on=["variable", "stream"], how='left')
 
 
 catalogue = load_catalogue()
@@ -172,6 +172,10 @@ def load_all(cat: pandas.DataFrame = catalogue, **kwargs):
 
         ds[var].attrs["resolution"] = res
         ds[var].attrs["stream"] = stream
+
+        # Remove time from fx variables
+        if stream == 'fx':
+            ds = ds.squeeze(['time', 'ensemble'], drop=True)
 
         results[name] = ds
 
